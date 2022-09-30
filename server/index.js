8e65338be14a4de8ba43a6c47eec7b291cd36b3a
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2022-09-30 23:32:18 +0800

    chore: fix license comment

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 /**
  *  YouTube Downloader
- *  Copyright (C) 2020 - 2022, Adriane Justine Ta
-      n
+ *  Copyright (C) 2020 - 2022, Adriane Justine Tan
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
