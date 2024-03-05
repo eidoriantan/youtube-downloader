99c33058ac732c73afa6571f83a3960bd6ad2f0b
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2024-03-05 23:03:25 +0800

    feat: implement Electron

---
+++ forge.config.js
@@ -0,0 +1,52 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const path = require('node:path')
+
+const iconPath = path.join(__dirname, 'images/icon');
+const icoPath = iconPath + '.ico';
+
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    icon: iconPath
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        iconUrl: 'https://yt-downloader.eidoriantan.me/favicon.ico',
+        setupIcon: icoPath
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+};
---
+++ main.js
@@ -0,0 +1,120 @@
+/**
+ *  YouTube Downloader
+ *  Copyright (C) 2020 - 2024, Adriane Justine Tan
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const { app, shell, BrowserWindow, Menu } = require('electron')
+const path = require('node:path')
+require('./server/')
+
+const port = process.env.SERVER_PORT || 3001
+
+const createMenu = () => {
+  const isMac = process.platform === 'darwin'
+  const template = [
+    {
+      label: 'File',
+      submenu: [
+        { role: isMac ? 'close' : 'quit' }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forceReload' },
+        { type: 'separator' },
+        { role: 'resetZoom' },
+        { role: 'zoomIn' },
+        { role: 'zoomOut' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'zoom' },
+        ...(isMac
+          ? [
+              { type: 'separator' },
+              { role: 'front' },
+              { type: 'separator' },
+              { role: 'window' }
+            ]
+          : [
+              { role: 'close' }
+            ])
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'About Me',
+          click: async () => {
+            await shell.openExternal('https://eidoriantan.me')
+          }
+        }
+      ]
+    }
+  ]
+
+  return Menu.buildFromTemplate(template)
+}
+
+const createWindow = () => {
+  const icon = path.join(__dirname, 'public/favicon.ico')
+  const win = new BrowserWindow({
+    center: true,
+    icon,
+    webPreferences: {
+      devTools: false,
+      enableWebSQL: false
+    }
+  })
+
+  const menu = createMenu()
+  win.setMenu(menu)
+  win.loadURL(`http://localhost:${port}`)
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
